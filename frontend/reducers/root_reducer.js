7a9e806e78f5721347c9c5f0544e65e6fbfe3290
Stephen Chung <signup@stephenchung.me>
2019-01-26 14:56:24 -0500

    Fix rootReducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -3,6 +3,7 @@ import { combineReducers } from 'redux';
 import entitiesReducer from './entities_reducer';
 import sessionReducer from './session_reducer';
 import errorsReducer from './errors_reducer';
+import uiReducer from './ui_reducer';
 
 const rootReducer = combineReducers({
   entities: entitiesReducer,

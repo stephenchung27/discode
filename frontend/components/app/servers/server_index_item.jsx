a68f5a6000b1f10c7d1074bf1f011b3a42d28b3d
Stephen Chung <signup@stephenchung.me>
2019-02-21 15:31:58 -0500

    Remove notifications

--- frontend/components/app/servers/server_index_item.jsx
+++ frontend/components/app/servers/server_index_item.jsx
@@ -8,7 +8,7 @@ const ServerIndexItem = ({ server, match }) => {
   return (
     <li style={avatarURL} className={"server-item" + (match.params.serverPath === server.path ? " active-server" : "")}>
       {avatarURL ? null : server.server_name[0]}
-      <div className="server-notifications">9</div>
+      {/* <div className="server-notifications">9</div> */}
       <div className="server-name">{server.server_name}</div>
     </li>
   )
